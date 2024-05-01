685f89a177c3625f2b383c8c10684cad24f0d5b0
jayani0425 <141249125+jayani0425@users.noreply.github.com>
2024-05-02 04:48:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
